9886aba2bc4c73ea31f4bc5ce098b16bdb643024
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-15 15:26:38 +0800

    fix casing name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header.jsx";
 import Login from "./components/Login.jsx";
-import StateLogin from "./components/StateLogin.jsx";
+import StateLogin from "./components/stateLogin.jsx";
 import Signup from "./components/Signup.jsx";
 
 function App() {
